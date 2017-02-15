6169267d2a694d3bf01dc457dc7b75900f2b6879
oba <basista@fg.cz>
2017-02-15 13:47:49 +0100

    add prev version history controll

--- js/HistoryMem.js
+++ js/HistoryMem.js
@@ -1,10 +1,35 @@
 class HistoryMem {
     constructor() {
       this.coreHistory = [];
+
+      //controlls
+      this.nextVersionSubmit = null;
+      this.prevVersionSubmit = null;
+
+      this.localInitSettings();
     }
 
     localInitSettings() {
         var t = this;
+        t.controlDefinition();
+        t.prevVersion();
+    }
+
+    controlDefinition(){
+      var t = this;
+      t.nextVersionSubmit = $('.js_next-version-button');
+      t.prevVersionSubmit = $('.js_prev-version-button');
+    }
+
+
+    prevVersion() {
+      var t = this;
+      t.prevVersionSubmit.on('click', function () {
+        console.log('ehm');
+        var appCoreState = dataTranslator.getCoreStructureHolder();
+        appCoreState.pop();
+        dataTranslator.rerenderPreview();
+      });
     }
 
     addHistory(historyArray){
